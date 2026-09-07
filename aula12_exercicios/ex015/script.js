cb68b914875e5c781105810e32c20d91c5c99801
rafael casagrande <rafaehlcasagrande@gmail.com>
2026-09-07 11:31:22 -0300

    Definindo comportamento por sexo

---
+++ aula12_exercicios/ex015/script.js
@@ -0,0 +1,52 @@
+function verificar() {
+  var corpo = document.getElementById("corpo");
+  var data = new Date();
+  var ano = data.getFullYear();
+  var formAno = document.getElementById("txtano");
+  var msg2 = document.getElementById("msg2");
+  var moldura = document.getElementById("moldura");
+  var res = document.querySelector("img#res");
+  var fsex = document.getElementsByName("radsex");
+  var idade = ano - Number(formAno.value);
+  var genero = "";
+  if (formAno.value.length == 0 || formAno.value > ano) {
+    return alert(`[ERRO] Verifique os dados preenchidos e tente novamente`);
+  }
+  moldura.style.visibility = "visible";
+  moldura.style.width = "250px";
+  moldura.style.height = "250px";
+  if (fsex[0].checked) {
+    corpo.style.backgroundColor = "#3934e0";
+    genero = "Masculino";
+    if (idade <= 4) {
+      res.src = "imgs/bebemenino.jpg";
+    } else if (idade <= 12) {
+      res.src = "imgs/criancamenino.jpg";
+    } else if (idade <= 18) {
+      res.src = "imgs/adolescentemenino.jpg";
+    } else if (idade <= 29) {
+      res.src = "imgs/jovemhomen.jpg";
+    } else if (idade < 60) {
+      res.src = "imgs/adultohomem.jpg";
+    } else {
+      res.src = "imgs/homem-idoso.jpg";
+    }
+    return (msg2.innerHTML = `<p> Sua idade é de ${idade} anos<br> e seu sexo é ${genero}`);
+  }
+  genero = " Feminino";
+  corpo.style.backgroundColor = "#f7daa8";
+  if (idade <= 4) {
+    res.src = "imgs/bebemenina.jpg";
+  } else if (idade <= 12) {
+    res.src = "imgs/criancamenina.jpg";
+  } else if (idade <= 18) {
+    res.src = "imgs/adolescentemenina.jpg";
+  } else if (idade <= 29) {
+    res.src = "imgs/jovemmulher.jpg";
+  } else if (idade < 60) {
+    res.src = "imgs/adultomulher.jpg";
+  } else {
+    res.src = "imgs/mulher-idosa.jpg";
+  }
+  return (msg2.innerHTML = `<p> Sua idade é de ${idade} anos<br> e seu sexo é ${genero}`);
+}
