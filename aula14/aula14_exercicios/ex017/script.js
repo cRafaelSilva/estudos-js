5ab90098333ddff37c7568e6a60a55d395061db0
rafael casagrande <rafaehlcasagrande@gmail.com>
2026-09-11 21:53:08 -0300

    resolucao ex02

--- aula14/aula14_exercicios/ex017/script.js
+++ aula14/aula14_exercicios/ex017/script.js
@@ -1,1 +1,16 @@
-alert("Olá");
+function gerarTabuada() {
+  let num = document.getElementById("num");
+  let selectTab = document.getElementById("seltab");
+  if (num.value.length == 0) {
+    window.alert("Por favor, digite um número!");
+  } else {
+    num = Number(num.value);
+    selectTab.innerHTML = "";
+    for (let c = 0; c <= 10; c++) {
+      let item = document.createElement("option");
+      item.text = `${num} x ${c} = ${num * c}`;
+      item.value = `tab${c}`;
+      selectTab.appendChild(item);
+    }
+  }
+}
